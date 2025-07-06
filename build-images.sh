f1b9cd51515a70eb387c6767aff4db3b56f6599f
mrmarkuz <mrmarkuz666@gmail.com>
2025-07-06 22:10:18 +0200

    Update to 3.1.0

--- build-images.sh
+++ build-images.sh
@@ -39,7 +39,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/opencloudeu/opencloud-rolling:2.2.0" \
+    --label="org.nethserver.images=docker.io/opencloudeu/opencloud-rolling:3.1.0" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
